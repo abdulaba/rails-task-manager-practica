72b85b59d832daf7f5756253aa1ca263a3567b62
andres ibanez <andresibanezulloa@gmail.com>
2023-08-29 10:19:50 -0400

    add cdn bootstrap, routes resources tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
-  patch "tasks/:id", to: "tasks#update"
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  post "tasks", to: "tasks#create"
-  get "tasks/new", to: "tasks#new", as: :new_task
-  get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks", to: "tasks#index"
+  root to: "tasks#index"
+  resources :tasks
 end
